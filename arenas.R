15e0a55d7dc09b8de629e2df4723e888909c7c05
Dennis Evangelista <devangel77b@gmail.com>
2024-11-30 20:17:40 -0500

    fixing correspondence author and () in figures

--- arenas.R
+++ arenas.R
@@ -14,8 +14,8 @@ apredicted.ms2 <- function(m2,m1,mc){
 fig <- ggplot(data)+geom_point(aes(x=m2.kg,y=ameas.ms2))+
     ylim(0,2)+xlim(0.02,0.5)+
     geom_function(fun=apredicted.ms2,args=list(m1=1.000,mc=0.5),color='blue')+
-    xlab('$m_2$, \\unit{\\kilo\\gram}')+
-    ylab('$a$, \\unit{\\meter\\per\\second\\squared}')+
+    xlab('$m_2$ (\\unit{\\kilo\\gram})')+
+    ylab('$a$ (\\unit{\\meter\\per\\second\\squared})')+
     theme_bw(base_size=8)
 
 library(svglite)
